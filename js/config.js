bec4c89c54833fd29be042fc8b3ee96fd5156ed0
xuejike <xuejike2004@126.com>
2016-11-23 15:48:44 +0800

    feat: 订单的套系金额完成

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ if(!PROD){ // 测试环境
   //API_BASE = 'http://118.178.226.161:8082/wxy/inf/',
   API_BASE = 'http://127.0.0.1:8081/wxy/inf/',
   //USER_OPENID = 'okju2w7Rt607heeud2joDzfPpBmQ';
-  USER_OPENID = 'okju2w9qlr7oq7nmv5R_EfsMSO5A';
+  USER_OPENID = 'okju2w_xk9qpjXgEeDF2pu3i3c9A';
   // 抓客 okju2w9qlr7oq7nmv5R_EfsMSO5A
   // 邀约 okju2wyrqbNNeNsYLWNpPGaJZ6FU
   // 门市 okju2w-cU1cIABNSa4x9Np6WEo4k
